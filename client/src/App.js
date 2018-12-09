b75b34e952d3b2ae7cde0404f2993a9278f35b5e
kwalaszczyk <selthias@gmail.com>
2018-12-09 20:52:59 +0100

    Add client catalog structure, add dependencies

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <div className="container" />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
