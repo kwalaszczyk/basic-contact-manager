8b5cadd57ab7997e66ea0a6fa4c7b06738922a8b
kwalaszczyk <selthias@gmail.com>
2018-12-09 18:49:07 +0100

    Change POST response status to 201, add Location header

--- src/main/java/com/walaszczyk/contactmanager/controller/ContactsController.java
+++ src/main/java/com/walaszczyk/contactmanager/controller/ContactsController.java
@@ -3,8 +3,11 @@ package com.walaszczyk.contactmanager.controller;
 import com.walaszczyk.contactmanager.domain.Contact;
 import com.walaszczyk.contactmanager.exceptions.ContactNotFoundException;
 import com.walaszczyk.contactmanager.service.ContactService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,8 +31,12 @@ public class ContactsController {
     }
 
     @PostMapping()
-    public Contact addContact(@RequestBody Contact newContact) {
-        return contactService.addContact(newContact);
+    public ResponseEntity<?> addContact(@RequestBody Contact newContact) {
+        Contact addedContact = contactService.addContact(newContact);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(addedContact.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping(value = "/{id}")
