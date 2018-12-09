93cc5df574b06625749cfde0ce7c76607f3f7c9e
kwalaszczyk <selthias@gmail.com>
2018-12-09 21:17:26 +0100

    Dispatch action GET_CONTACTS to reducer

--- client/src/actions/contactActions.js
+++ client/src/actions/contactActions.js
@@ -4,5 +4,8 @@ import axios from "axios";
 
 export const getContacts = () => async dispatch => {
   const res = await axios.get("http://localhost:8080/api/contacts");
-  console.log(res.data);
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
 };
--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -16,7 +16,9 @@ class Contacts extends Component {
         <h1 className="display-4 mb-2">
           <span className="text-primary">Contact</span> List
         </h1>
-        {contacts.forEach(console.log)}
+        {contacts.map(contact => (
+          <div>{contact["name"]}</div>
+        ))}
       </React.Fragment>
     );
   }
