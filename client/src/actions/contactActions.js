f77298eb03a45942a4a33e108dcc214be9ebbd8b
kwalaszczyk <selthias@gmail.com>
2018-12-10 14:45:40 +0100

    Set proxy

--- client/src/actions/contactActions.js
+++ client/src/actions/contactActions.js
@@ -3,7 +3,7 @@ import { GET_CONTACTS } from "./types";
 import axios from "axios";
 
 export const getContacts = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/contacts");
+  const res = await axios.get("/api/contacts");
   dispatch({
     type: GET_CONTACTS,
     payload: res.data
