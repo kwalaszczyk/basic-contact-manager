73b98b7acafa33b14202611c1ba6195f2a668278
kwalaszczyk <selthias@gmail.com>
2018-12-10 16:15:36 +0100

    EditContact component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import Contacts from "./components/contacts/Contacts";
+import EditContact from "./components/contacts/EditContact";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -15,6 +16,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
               </Switch>
             </div>
           </div>
